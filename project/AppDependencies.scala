3610d2cccee8ba74b5275f4dab5247e9d3314b12
Lee Powell <lee.powell@digital.hmrc.gov.uk>
2024-02-15 12:52:37 +0000

    VEIOSS-540 | Upgrade to Play 3.0

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,25 +3,24 @@ import sbt.*
 
 object AppDependencies {
 
-  private val bootstrapVersion = "7.23.0"
-  private val hmrcMongoVersion = "1.4.0"
+  private val bootstrapVersion = "8.4.0"
+  private val hmrcMongoVersion = "1.7.0"
 
   val compile = Seq(
-    "uk.gov.hmrc" %% "bootstrap-backend-play-28" % bootstrapVersion,
-    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28" % hmrcMongoVersion,
-    "uk.gov.hmrc" %% "domain" % "8.1.0-play-28"
+    "uk.gov.hmrc"       %% "bootstrap-backend-play-30"  % bootstrapVersion,
+    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"         % hmrcMongoVersion,
+    "uk.gov.hmrc"       %% "domain-play-30"             % "9.0.0"
   )
 
   val test = Seq(
-    "uk.gov.hmrc" %% "bootstrap-test-play-28" % bootstrapVersion,
-    "uk.gov.hmrc.mongo" %% "hmrc-mongo-test-play-28" % hmrcMongoVersion,
-    "org.scalatest" %% "scalatest" % "3.2.12",
-    "com.typesafe.play" %% "play-test" % PlayVersion.current,
-    "com.vladsch.flexmark" % "flexmark-all" % "0.62.2",
-    "org.scalatestplus.play" %% "scalatestplus-play" % "5.1.0",
-    "org.scalatestplus" %% "scalacheck-1-15" % "3.2.11.0",
-    "org.scalatestplus" %% "mockito-3-4" % "3.2.10.0",
-    "org.mockito" %% "mockito-scala" % "1.17.7",
-    "com.github.tomakehurst" % "wiremock-standalone" % "2.27.2"
+    "uk.gov.hmrc"             %% "bootstrap-test-play-30"   % bootstrapVersion,
+    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-test-play-30"  % hmrcMongoVersion,
+    "org.scalatest"           %% "scalatest"                % "3.2.15",
+    "org.playframework"       %% "play-test"                % PlayVersion.current,
+    "com.vladsch.flexmark"    % "flexmark-all"              % "0.64.6",
+    "org.scalatestplus.play"  %% "scalatestplus-play"       % "5.1.0",
+    "org.scalatestplus"       %% "scalacheck-1-15"          % "3.2.11.0",
+    "org.scalatestplus"       %% "mockito-4-6"              % "3.2.15.0",
+    "org.mockito"             %% "mockito-scala"            % "1.17.30"
   ).map(_ % "test, it")
 }
